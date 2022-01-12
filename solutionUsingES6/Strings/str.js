77b36ea1978e42f4505b236c2664c52ec3ffdeec
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-01-12 23:34:26 +0530

    "String started"

---
+++ solutionUsingES6/Strings/str.js
@@ -0,0 +1,19 @@
+
+//uppercase
+let req = require("readline-sync")
+// console.log(req.question("enter the string").toUpperCase())
+
+// console.log(req.question("enter first")+req.question("enter second"))
+
+// console.log(req.question("enter str to cal length").length)
+
+// console.log(typeof parseInt(req.question("enter no")))
+
+str = req.question("enter string to remove vowel ")
+res = ""
+vowels = ['a','e','o','u','i']
+vowels = 'aeiou'
+console.log(vowels.includes(str[1]))
+for(let i=0;i<str.length;i++){
+    
+}
